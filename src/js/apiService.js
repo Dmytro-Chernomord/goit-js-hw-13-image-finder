b94103e5dcc83b510d93fb2b5ed89e2e2ed5157f
Dmytro Chernomord <tschernomord@gmail.com>
2020-08-19 09:44:20 +0200

    add "not found"

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,31 +1,44 @@
 import refs from './refs';
 import card from '../template/card.hbs';
-import { nextPage, page } from './nextPage.js';
+// import { nextPage } from './nextPage.js';
 import scroll from './scrol.js';
 
 require('lodash');
 const apiKey = `17932091-a96e694bd6f62b0fd0558b77b`;
-
+let page = 1;
 refs.input.addEventListener('input', _.debounce(search, 500));
 
 async function search() {
   let searchQuery = refs.input.value;
   const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${page}&per_page=12&key=${apiKey}`;
+
   const height = refs.ul.clientHeight;
 
-  refs.searchBtn.classList.remove('isHiden');
   refs.searchBtn.addEventListener('click', nextPage);
 
   scroll(height);
   if (searchQuery) {
     const respPict = await fetch(url);
     const dataPars = await respPict.json();
-    refs.ul.insertAdjacentHTML('beforeend', card(dataPars.hits));
+    console.log(dataPars.hits.length);
+    if (dataPars.hits.length === 0) {
+      refs.ul.innerHTML = 'Not found';
+    } else {
+      refs.ul.insertAdjacentHTML('beforeend', card(dataPars.hits));
+      refs.searchBtn.classList.remove('isHiden');
+    }
   } else {
+    page = 1;
     refs.ul.innerHTML = '';
     refs.searchBtn.removeEventListener('click', nextPage);
+
     refs.searchBtn.classList.add('isHiden');
   }
 }
 
-export default search;
+function nextPage() {
+  page += 1;
+
+  refs.searchBtn.removeEventListener('click', nextPage);
+  search();
+}
