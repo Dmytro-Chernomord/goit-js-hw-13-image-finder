8c9f4cdd214c8570c645e1cff30ce46b563c5d6e
Dmytro <tschernomord@gmail.com>
2021-01-08 15:28:55 +0100

    change form

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 import './styles.css';
 import './js/apiService.js';
+// const form = document.querySelector('div');
+// console.log(form);
+// form.addEventListener('input', (e) => e.preventDefault())
